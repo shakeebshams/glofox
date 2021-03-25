f2fec85165ea19a8d4ddea80c348889899a044b3
shakeebshams <shakeebshams@gmail.com>
2021-03-24 22:45:01 -0400

    init commit

---
+++ api/api.go
@@ -0,0 +1,17 @@
+package api
+
+import (
+    "fmt"
+    "log"
+    "net/http"
+)
+
+func homePage(w http.ResponseWriter, r *http.Request){
+    fmt.Fprintf(w, "Welcome to the HomePage!")
+    fmt.Println("Endpoint Hit: homePage")
+}
+
+func handleRequests() {
+    http.HandleFunc("/", homePage)
+    log.Fatal(http.ListenAndServe(":8080", nil))
+}
---
+++ main.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+	"api/api.go"
+)
+
+func main() {
+    handleRequests()
+}
